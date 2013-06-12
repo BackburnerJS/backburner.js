75619fe6de921584e5c6526f4f645cf927f6cc3f
Tim Evans <tim-evans@sproutcore.com>
2013-06-11 23:31:57 -0400

    rename debounce to throttle, as that's what it was doing

--- test/tests/defer_test.js
+++ test/tests/defer_test.js
@@ -196,54 +196,54 @@ test("autorun", function() {
   stop();
 });
 
-test("debounce", function() {
+test("throttle", function() {
   expect(14);
 
   var bb = new Backburner(['zomg']),
       step = 0;
 
   var wasCalled = false;
-  function debouncee() {
+  function throttler() {
     ok(!wasCalled);
     wasCalled = true;
   }
 
-  // let's debounce the function `debouncee` for 40ms
+  // let's throttle the function `throttler` for 40ms
   // it will be executed in 40ms
-  bb.debounce(null, debouncee, 40);
+  bb.throttle(null, throttler, 40);
   equal(step++, 0);
 
-  // let's schedule `debouncee` to run in 10ms
+  // let's schedule `throttler` to run in 10ms
   stop();
   setTimeout(function() {
     start();
     equal(step++, 1);
     ok(!wasCalled);
-    bb.debounce(null, debouncee);
+    bb.throttle(null, throttler);
   }, 10);
 
-  // let's schedule `debouncee` to run again in 20ms
+  // let's schedule `throttler` to run again in 20ms
   stop();
   setTimeout(function() {
     start();
     equal(step++, 2);
     ok(!wasCalled);
-    bb.debounce(null, debouncee);
+    bb.throttle(null, throttler);
   }, 20);
 
-  // let's schedule `debouncee` to run yet again in 30ms
+  // let's schedule `throttler` to run yet again in 30ms
   stop();
   setTimeout(function() {
     start();
     equal(step++, 3);
     ok(!wasCalled);
-    bb.debounce(null, debouncee);
+    bb.throttle(null, throttler);
   }, 30);
 
-  // at 40ms, `debouncee` will get called once
+  // at 40ms, `throttler` will get called once
 
   // now, let's schedule an assertion to occur at 50ms,
-  // 10ms after `debouncee` has been called
+  // 10ms after `throttler` has been called
   stop();
   setTimeout(function() {
     start();
@@ -252,11 +252,11 @@ test("debounce", function() {
   }, 50);
 
   // great, we've made it this far, there's one more thing
-  // we need to test. we want to make sure we can call `debounce`
+  // we need to test. we want to make sure we can call `throttle`
   // again with the same target/method after it has executed
 
 
-  // at the 60ms mark, let's schedule another call to `debounce`
+  // at the 60ms mark, let's schedule another call to `throttle`
   stop();
   setTimeout(function() {
     wasCalled = false; // reset the flag
@@ -265,15 +265,15 @@ test("debounce", function() {
     start();
     equal(step++, 5);
 
-    // call debounce for the second time
-    bb.debounce(null, debouncee, 100);
+    // call throttle for the second time
+    bb.throttle(null, throttler, 100);
 
     // assert that it is called in the future and not blackholed
     stop();
     setTimeout(function() {
       start();
       equal(step++, 6);
-      ok(wasCalled, "Another debounce call with the same function can be executed later");
+      ok(wasCalled, "Another throttle call with the same function can be executed later");
     }, 110);
   }, 60);
 });
