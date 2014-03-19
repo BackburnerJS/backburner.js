BackburnerJS/backburner.js

commit 03dd5cd52825d41ee4ee82bf2bc762de1f2a4413

Added onError

lib/backburner/deferred_action_queues.js
@@ -4,16 +4,19 @@ function DeferredActionQueues(queueNames, options) {
   var queues = this.queues = {};
   this.queueNames = queueNames = queueNames || [];
 
+  this.options = options;
+
   var queueName;
   for (var i = 0, l = queueNames.length; i < l; i++) {
     queueName = queueNames[i];
-    queues[queueName] = new Queue(this, queueName, options[queueName]);
+    queues[queueName] = new Queue(this, queueName, this.options);
   }
 }
 
 DeferredActionQueues.prototype = {
   queueNames: null,
   queues: null,
+  options: null,
 
   schedule: function(queueName, target, method, args, onceFlag, stack) {
     var queues = this.queues,
@@ -44,6 +47,7 @@ DeferredActionQueues.prototype = {
       var options = queue.options,
           before = options && options.before,
           after = options && options.after,
+          onError = this.options.onError,
           target, method, args, stack,
           queueIndex = 0, numberOfQueueItems = queueItems.length;
 
@@ -60,9 +64,25 @@ DeferredActionQueues.prototype = {
         if (method) {
           // TODO: error handling
           if (args && args.length > 0) {
-            method.apply(target, args);
+            if (onError) {
+              try {
+                method.apply(target, args);
+              } catch (e) {
+                onError(e);
+              }
+            } else {
+              method.apply(target, args);
+            }
           } else {
-            method.call(target);
+            if (onError) {
+              try {
+                method.call(target);
+              } catch(e) {
+                onError(e);
+              }
+            } else {
+              method.call(target);
+            }
           }
         }
 

lib/backburner.js
@@ -76,13 +76,33 @@ Backburner.prototype = {
       method = target[method];
     }
 
+    var onError = this.options.onError;
+
     // Prevent Safari double-finally.
     var finallyAlreadyCalled = false;
     try {
       if (arguments.length > 2) {
-        ret = method.apply(target, slice.call(arguments, 2));
+        if (onError) {
+          // Do we need this double try?
+          try {
+            ret = method.apply(target, slice.call(arguments, 2));
+          } catch (e) {
+            onError(e);
+          }
+        } else {
+          ret = method.apply(target, slice.call(arguments, 2));
+        }
       } else {
-        ret = method.call(target);
+        if (onError) {
+          // Do we need this double try?
+          try {
+            ret = method.call(target);
+          } catch (e) {
+            onError(e);
+          }
+        } else {
+          ret = method.call(target);
+        }
       }
     } finally {
       if (!finallyAlreadyCalled) {
@@ -180,8 +200,18 @@ Backburner.prototype = {
       method = target[method];
     }
 
+    var onError = this.options.onError;
+
     function fn() {
-      method.apply(target, args);
+      if (onError) {
+        try {
+          method.apply(target, args);
+        } catch (e) {
+          onError(e);
+        }
+      } else {
+        method.apply(target, args);
+      }
     }
 
     // find position to insert

lib/backburner/queue.js
@@ -9,6 +9,7 @@ Queue.prototype = {
   daq: null,
   name: null,
   options: null,
+  onError: null,
   _queue: null,
 
   push: function(target, method, args, stack) {
@@ -27,7 +28,7 @@ Queue.prototype = {
       if (currentTarget === target && currentMethod === method) {
         queue[i+2] = args; // replace args
         queue[i+3] = stack; // replace stack
-        return {queue: this, target: target, method: method}; 
+        return {queue: this, target: target, method: method};
       }
     }
 
@@ -38,9 +39,10 @@ Queue.prototype = {
   // TODO: remove me, only being used for Ember.run.sync
   flush: function() {
     var queue = this._queue,
-        options = this.options,
+        options = this.options[this.name],
         before = options && options.before,
         after = options && options.after,
+        onError = this.options.onError,
         target, method, args, stack, i, l = queue.length;
 
     if (l && before) { before(); }
@@ -52,9 +54,25 @@ Queue.prototype = {
 
       // TODO: error handling
       if (args && args.length > 0) {
-        method.apply(target, args);
+        if (onError) {
+          try {
+            method.apply(target, args);
+          } catch (e) {
+            onError(e);
+          }
+        } else {
+          method.apply(target, args);
+        }
       } else {
-        method.call(target);
+        if (onError) {
+          try {
+            method.call(target);
+          } catch(e) {
+            onError(e);
+          }
+        } else {
+          method.call(target);
+        }
       }
     }
     if (l && after) { after(); }

test/tests/debounce_test.js
@@ -302,3 +302,18 @@ test("debounce that is immediate, and cancelled and called again happens immedia
   }, 10);
 
 });
+
+test("onError", function() {
+  expect(1);
+
+  function onError(error) {
+    equal("test error", error.message);
+    start();
+  }
+
+  var bb = new Backburner(['errors'], { onError: onError });
+
+  bb.debounce(null, function() { throw new Error("test error"); }, 20);
+
+  stop();
+});

test/tests/defer_once_test.js
@@ -50,7 +50,7 @@ test("when passed a target, method, and arguments", function() {
   });
 
   ok(functionWasCalled, "function was called");
-}); 
+});
 
 test("when passed same function twice", function() {
   expect(2);
@@ -177,5 +177,21 @@ test("when passed same function twice with different target and different argume
   });
 
   equal(i, 2, "function was called twice");
+});
+
+test("onError", function() {
+  expect(1);
+
+  function onError(error) {
+    equal("test error", error.message);
+  }
+
+  var bb = new Backburner(['errors'], { onError: onError });
+
+  bb.run(function() {
+    bb.deferOnce('errors', function() {
+      throw new Error("test error");
+    });
+  });
 });
 

test/tests/defer_test.js
@@ -162,3 +162,19 @@ test("when passed same function twice with different target and different argume
 
   equal(i, 2, "function was called twice");
 });
+
+test("onError", function() {
+  expect(1);
+
+  function onError(error) {
+    equal("test error", error.message);
+  }
+
+  var bb = new Backburner(['errors'], { onError: onError });
+
+  bb.run(function() {
+    bb.defer('errors', function() {
+      throw new Error("test error");
+    });
+  });
+});

test/tests/run_test.js
@@ -111,3 +111,29 @@ test("runs can be nested", function() {
     });
   });
 });
+
+test("onError", function() {
+  expect(1);
+
+  function onError(error) {
+    equal("test error", error.message);
+  }
+
+  var bb = new Backburner(['errors'], { onError: onError });
+
+  bb.run(function() { throw new Error("test error"); });
+});
+
+test("onError set after start", function() {
+  expect(2);
+
+  var bb = new Backburner(['errors']);
+
+  bb.run(function() { ok(true); });
+
+  bb.options.onError = function(error) {
+    equal("test error", error.message);
+  };
+
+  bb.run(function() { throw new Error("test error"); });
+});

test/tests/set_timeout_test.js
@@ -208,3 +208,18 @@ test("[obj, string, value, numericString]", function(){
     }
   }, 'bro', 'value', '1');
 });
+
+test("onError", function() {
+  expect(1);
+
+  function onError(error) {
+    equal("test error", error.message);
+    start();
+  }
+
+  var bb = new Backburner(['errors'], { onError: onError });
+
+  bb.setTimeout(function() { throw new Error("test error"); }, 1);
+
+  stop();
+});

commit b2efb58a8c03b84c4e44086e27bff4dcad1c1272

Fix bug introduced in 03dd5cd52825d41ee4ee82bf2bc762de1f2a4413 that was causing queue options to not probably get used
TODO: we should write a test for this

lib/backburner/deferred_action_queues.js
@@ -44,7 +44,7 @@ DeferredActionQueues.prototype = {
       queueItems = queue._queueBeingFlushed = queue._queue.slice();
       queue._queue = [];
 
-      var options = queue.options,
+      var options = queue.options[queue.name], // TODO: write a test for this
           bbOptions = this.options,
           before = options && options.before,
           after = options && options.after,