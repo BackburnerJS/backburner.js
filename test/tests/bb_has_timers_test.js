ec4b50c31eff33ec9d324237e2675cb34f76e0e8
David J. Hamilton <davidjh@hjdivad.com>
2014-04-01 17:18:54 -0700

    hasTimers checks debouncees and throttlers.
    This ensures that ember-testing async helpers wait on debounced and throttled
    functions as well as other timers.

---
+++ test/tests/bb_has_timers_test.js
@@ -0,0 +1,36 @@
+import { Backburner } from "backburner";
+
+module("hasTimers");
+
+test("hasTimers", function () {
+  var bb = new Backburner(['ohai']),
+      timer,
+      target = {
+        fn: function () {}
+      };
+
+  bb.schedule('ohai', null, function () {
+    ok(!bb.hasTimers(), "Initially there are no timers");
+    start();
+
+    timer = bb.later('ohai', function() {});
+    ok(bb.hasTimers(), "hasTimers checks timers");
+
+    bb.cancel(timer);
+    ok(!bb.hasTimers(), "Timers are cleared");
+
+    timer = bb.debounce(target, 'fn', 200);
+    ok(bb.hasTimers(), "hasTimers checks debouncees");
+
+    bb.cancel(timer);
+    ok(!bb.hasTimers(), "Timers are cleared");
+
+    timer = bb.throttle(target, 'fn', 200);
+    ok(bb.hasTimers(), "hasTimers checks throttlers");
+
+    bb.cancel(timer);
+    ok(!bb.hasTimers(), "Timers are cleared");
+  });
+
+  stop();
+});
