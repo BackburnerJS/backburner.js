11962ebbd822b2e674379f942ead003eff7b0cad
Stanley Stuart <stanley@stan.li>
2014-08-16 00:29:26 -0500

    pass stack debugging info to onError function

--- test/tests/debug_test.js
+++ test/tests/debug_test.js
@@ -14,7 +14,22 @@ test("DEBUG flag enables stack tagging", function() {
   bb.defer('one', function() {});
 
   if (new Error().stack) { // workaround for CLI runner :(
+    expect(4);
     var stack = bb.currentInstance.queues.one._queue[7].stack;
     ok(typeof stack === 'string', "A stack is recorded");
+
+    var onError = function(error, errorRecordedForStack){
+      ok(errorRecordedForStack, 'errorRecordedForStack passed to error function');
+      ok(errorRecordedForStack.stack, 'stack is recorded');
+    };
+
+    bb = new Backburner(['errors'], {onError: onError});
+    bb.DEBUG = true;
+
+    bb.run(function(){
+      bb.defer('errors', function(){
+        throw new Error('message!');
+      });
+    });
   }
 });
