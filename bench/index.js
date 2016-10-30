29d4893bdecdc1e2443b2987b4bc53e064509bf3
Robert Jackson <rwjblue@twitch.tv>
2016-10-30 14:44:47 -0400

    Add basic benchmarks for cancel.
    In preparation for playing with `run.cancel` performance I figured I'd
    add some benchmarks to be able to quickly judge before/after.
    
    Current output of `npm run bench` is:
    
    ```
      Cancel - no timer ............................................................... 27,841,173.65 op/s
      Schedule & Cancel - function ..................................................... 3,261,977.70 op/s
      Schedule & Cancel - target, function ............................................. 3,226,585.72 op/s
      Schedule & Cancel - target, string method name ................................... 3,173,040.97 op/s
      Schedule & Cancel - target, string method name, 1 argument ....................... 2,832,891.59 op/s
      Schedule & Cancel - target, string method name, 2 arguments ...................... 2,910,038.74 op/s
      Schedule & Cancel - prescheduled, same queue - target, string method name ........ 1,095,261.55 op/s
      Schedule & Cancel - prescheduled, separate queue - target, string method name .... 3,238,070.56 op/s
    ```

---
+++ bench/index.js
@@ -0,0 +1,15 @@
+var glob = require('glob');
+var path = require('path');
+var bench = require('do-you-even-bench');
+
+var suites = [];
+glob.sync( './bench/benches/*.js' ).forEach(function(file) {
+  var exported = require( path.resolve( file ) );
+  if (Array.isArray(exported)) {
+    suites = suites.concat(exported);
+  } else {
+    suites.push(exported);
+  }
+});
+
+bench(suites);
