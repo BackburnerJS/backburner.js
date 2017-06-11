2eaace739825e753be2a871d8d5f58bec0fb5963
bekzod <bekzod@me.com>
2017-06-12 02:13:40 +0500

    reverted test case

--- lib/index.ts
+++ lib/index.ts
@@ -78,8 +78,8 @@ export default class Backburner {
     this._platform = this.options._platform || {};
     this._platform.setTimeout = this._platform.setTimeout || ((fn, ms) => setTimeout(fn, ms));
     this._platform.clearTimeout = this._platform.clearTimeout || ((id) => clearTimeout(id));
-    this._platform.next = this._platform.next || ((fn) => setTimeout(fn, 0));
-    this._platform.clearNext = this._platform.clearNext || ((id) => clearTimeout(id));
+    this._platform.next = this._platform.next || ((fn) => this._platform.setTimeout(fn, 0));
+    this._platform.clearNext = this._platform.clearNext || this._platform.clearTimeout;
 
     this._boundRunExpiredTimers = () => {
       this._runExpiredTimers();
--- tests/configurable-timeout-test.ts
+++ tests/configurable-timeout-test.ts
@@ -36,11 +36,11 @@ QUnit.test('We can use a custom later', function(assert) {
     }
   });
 
-  bb.later(() => {
+  bb.scheduleOnce('one', () => {
     assert.ok(bb.options._platform.isFakePlatform, 'we are using the fake platform');
     assert.ok(customTimeoutWasUsed , 'custom later was used');
     done();
-  }, 0);
+  });
 });
 
 QUnit.test('We can use a custom clearTimeout', function(assert) {
