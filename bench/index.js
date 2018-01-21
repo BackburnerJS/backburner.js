08aebc0a81e4a168072ef1a0520c32e26993e82e
Robert Jackson <rjackson@linkedin.com>
2018-01-20 20:06:01 -0500

    Make it possible to run a single benchmark file.

--- bench/index.js
+++ bench/index.js
@@ -2,8 +2,21 @@ var glob = require('glob');
 var path = require('path');
 var bench = require('do-you-even-bench');
 
+// to run all (default):
+//
+// node ./bench/index.js
+//
+// to run a single benchmark, you can do:
+//
+// node ./bench/index.js ./bench/benches/some-file.js
+var fileGlob = './bench/benches/*.js';
+if (process.argv[2]) {
+  fileGlob = process.argv[2];
+  console.log(fileGlob);
+}
+
 var suites = [];
-glob.sync('./bench/benches/*.js').forEach(function(file) {
+glob.sync(fileGlob).forEach(function(file) {
   var exported = require( path.resolve( file ) );
   if (Array.isArray(exported)) {
     suites = suites.concat(exported);
