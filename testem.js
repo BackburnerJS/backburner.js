ce6b13e425713020cfbd146eca51eb83df79779a
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-02-05 01:03:26 +0100

    testem: Run Chrome with `--no-sandbox` on TravisCI

--- testem.js
+++ testem.js
@@ -5,10 +5,13 @@ module.exports = {
   launch_in_dev: ['Chrome'],
   browser_args: {
     Chrome: [
+      // --no-sandbox is needed when running Chrome inside a container
+      process.env.TRAVIS ? '--no-sandbox' : null,
+
       '--headless',
       '--disable-gpu',
       '--remote-debugging-port=9222',
       '--window-size=1440,90',
-    ],
+    ].filter(Boolean),
   },
 };
