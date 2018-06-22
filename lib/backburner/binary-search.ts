74e0bbee2d92260e5caaf5f59ce861432bf6b0d4
lifeart <lifeart92@gmail.com>
2018-06-22 03:22:34 +0300

    timers offset constant

--- lib/backburner/binary-search.ts
+++ lib/backburner/binary-search.ts
@@ -1,24 +1,26 @@
+import { TIMERS_OFFSET } from './utils';
+
 export default function binarySearch(time, timers) {
   let start = 0;
-  let end = timers.length - 6;
+  let end = timers.length - TIMERS_OFFSET;
   let middle;
   let l;
 
   while (start < end) {
     // since timers is an array of pairs 'l' will always
     // be an integer
-    l = (end - start) / 6;
+    l = (end - start) / TIMERS_OFFSET;
 
     // compensate for the index in case even number
     // of pairs inside timers
-    middle = start + l - (l % 6);
+    middle = start + l - (l % TIMERS_OFFSET);
 
     if (time >= timers[middle]) {
-      start = middle + 6;
+      start = middle + TIMERS_OFFSET;
     } else {
       end = middle;
     }
   }
 
-  return (time >= timers[start]) ? start + 6 : start;
+  return (time >= timers[start]) ? start + TIMERS_OFFSET : start;
 }
