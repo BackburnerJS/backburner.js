a0e412573e286c66e5f8246f12f786c12aba5f1e
David Taylor <david@taylorhq.com>
2022-10-06 23:29:24 +0100

    Ensure `scheduleOnce` works correctly following a cancelled job
    Previously, cancelling a job would `splice` it from the queue. This was problematic because it affects the index of all later jobs in the queue. The `targetQueues` maintains of jobs to queue indexes becomes incorrect, leading to arguments being passed to the wrong jobs. For example, given the sequence:
    
    ```javascript
    const toCancel = bb.scheduleOnce('queueName', null, f1, "f1 cancelled schedule");
    bb.scheduleOnce('queueName', null, f2, "f2 first schedule");
    bb.scheduleOnce('queueName', null, f3, "f3 first schedule");
    bb.cancel(toCancel);
    bb.scheduleOnce('queueName', null, f2, "f2 second schedule");
    ```
    
    The correct behaviour is for `f2` to be called with the argument `"f2 second schedule"`, and f3 to be called with `"f3 first schedule"`.
    
    In reality, `f2` was being called with the the argument from its first schedule ("f2 first schedule") and f3 was being called with the argument from f2's second schedule ("f2 second schedule").
    
    One possible fix would be to iterate over all `targetQueues` and decrement them by `QUEUE_ITEM_LENGTH` if they are greater than the cancelled job's index. That would be `O(n)` where `n` is the number of queued jobs. Instead, this commit nulls out out 'method' of the cancelled job in the queue, thereby making it a no-op (`O(1)`). That is the same technique employed a few lines below when cancelling the job in `_queueBeingFlushed`.

--- tests/cancel-test.ts
+++ tests/cancel-test.ts
@@ -18,6 +18,32 @@ QUnit.test('scheduleOnce', function(assert) {
   });
 });
 
+QUnit.test('cancelling does not affect future scheduleOnce calls', function(assert) {
+  assert.expect(5);
+
+  let bb = new Backburner(['queueName']);
+  const f1Calls: string[] = [];
+  const f2Calls: string[] = [];
+  const f3Calls: string[] = [];
+  const f1 = (arg: string) => f1Calls.push(arg);
+  const f2 = (arg: string) => f2Calls.push(arg);
+  const f3 = (arg: string) => f3Calls.push(arg);
+
+  bb.run(() => {
+    const toCancel = bb.scheduleOnce('queueName', null, f1, "f1 cancelled schedule");
+    bb.scheduleOnce('queueName', null, f2, "f2 first schedule");
+    bb.scheduleOnce('queueName', null, f3, "f3 first schedule");
+    bb.cancel(toCancel);
+    bb.scheduleOnce('queueName', null, f2, "f2 second schedule");
+  });
+
+  assert.equal(f1Calls.length, 0, "f1 was not called")
+  assert.equal(f2Calls.length, 1, "f2 was called once")
+  assert.equal(f3Calls.length, 1, "f3 was called once")
+  assert.deepEqual(f2Calls, ["f2 second schedule"], "f2 received the correct argument")
+  assert.deepEqual(f3Calls, ["f3 first schedule"], "f3 received the correct argument")
+});
+
 QUnit.test('setTimeout', function(assert) {
   assert.expect(5);
   let done = assert.async();
